e19920813230fa15c87ff27a9d39a21e4337c33f
Zoe Ames <zoeames@gmail.com>
2014-11-17 14:49:17 -0600

    file structure, webserver

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-/**
- * Created by zames on 11/17/14.
- */
+var port = process.env.PORT;
+var db = process.env.DB;
+var Hapi = require('hapi');
+var Joi = require('joi');
+var server = new Hapi.Server(port);
+
+var mongoose = require('mongoose');
+mongoose.connect(db);
+
+server.route({
+    config: {
+        description: 'this is the home page route'
+    },
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+
+server.route({
+    config: {
+        description: 'this is the about page route'
+    },
+    method: 'GET',
+    path: '/about',
+    handler: function (request, reply) {
+        reply('About!');
+    }
+});
+
+server.pack.register(
+    [
+        {
+            plugin: require('good'),
+            options: {
+                reporters: [{
+                    reporter: require('good-console'),
+                    args: [{log: '*', request: '*'}]
+                }]
+            }
+        },
+        {plugin: require('lout')}
+    ], function (err) {
+        if (err) {
+            throw err; // something bad happened loading the plugin
+        }
+
+        server.start(function () {
+            server.log('info', 'Server running at: ' + server.info.uri);
+        });
+    });
+
